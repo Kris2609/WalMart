0c4f02170fd8cf57201a71a571ff178659871e26
swaggerbee <fiske.unger@gmail.com>
2019-12-18 10:48:07 +0100

    test custumor

---
+++ WalMart/WalMart/TestCustomer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalMart.Astar;
+
+namespace WalMart
+{
+    class TestCustomer
+    {
+        CustomList<string> list = new CustomList<string>();
+        ShoppingList shoplist = new ShoppingList();
+
+        public TestCustomer()
+        {
+            getItems();
+        }
+        public CustomList<string> getItems()
+        {
+            if(list.Count() != null)
+            {
+                Random rand = new Random(500);
+
+                int temp = rand.Next(1, 7);
+                int temp2 = Convert.ToInt32(list.Count());
+                while (temp2 < temp)
+                {
+                    string tempnr = Convert.ToString(rand.Next(1, 21));
+                    // get tempitem = ShoppingList array item
+
+                    //string tempItem = shoplist.Array[tempnr]
+                   // if (!list.Contains(tempitem))
+                  //  {
+                    //    list.addItem(tempitem);
+                    //}
+                }
+
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+    }
+}
